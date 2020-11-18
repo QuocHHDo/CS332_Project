e9ae96522e0fb68486aeb66c3fcae5381afd8861
MdRashidulIkram <rashidulikram406@gmail.com>
2020-11-17 21:49:33 -0500

    Lets goooo

--- list.c
+++ list.c
@@ -5,6 +5,7 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <fcntl.h>
+#include <stdbool.h>
 
 #define READ_END 0
 #define WRITE_END 1
@@ -12,10 +13,11 @@
 int main(char **args) {
     system("clear"); //Clear Screen();
 
-    int file_desc = open("t1.txt", O_WRONLY | O_CREAT, 0666);
-    int pid_ls = fork(); // Fork a process to run ls -l
-
-    if (pid_ls < 0){
+    char input[32]; 
+    while (true){
+        int file_desc = open("t1.txt", O_WRONLY | O_CREAT, 0666);
+        int pid_ls = fork(); // Fork a process to run ls -l
+        if (pid_ls < 0){
         fprintf(stderr, "parent: Could not fork process to run ls\n");
         exit(0);
     } 
@@ -31,26 +33,33 @@ int main(char **args) {
         // because of an error
         fprintf(stderr, "child ls -l failed!\n");
         exit(0);
-    } else {
-        // Parent
-        close(file_desc);
-        // Wait for children to finish
-        wait(NULL);
-        // open file and print to stdout
-        FILE *file = fopen("t1.txt", "r");
-        if (file == NULL) {
-            printf("File t1.txt not found.\n");
-            exit(0);
-        }
-        char c;
-        c = fgetc(file);
-        while (c != EOF) {
-            printf("%c", c);
+        } else {
+            // Parent
+            close(file_desc);
+            // Wait for children to finish
+            wait(NULL);
+            // open file and print to stdout
+            FILE *file = fopen("t1.txt", "r");
+            if (file == NULL) {
+                printf("File t1.txt not found.\n");
+                exit(0);
+            }
+            char c;
             c = fgetc(file);
+            while (c != EOF) {
+                printf("%c", c);
+                c = fgetc(file);
+            }
+            fclose(file);
+            rename("t1.txt", "tree.txt"); // rename file
         }
-        fclose(file);
-        rename("t1.txt", "tree.txt"); // rename file
+        fgets(input,sizeof(input), stdin);      
+          if(input[0]=='\n')
+          {
+               return 0; 
+          }  
     }
+    
 }
 
 
