250289522b7c9c5908850d3e0adf16f2c7ef386f
Kelly Dong <dongweiling27@gmail.com>
2020-11-19 21:32:59 -0500

    final version of path

--- path.c
+++ path.c
@@ -6,94 +6,79 @@
 
 // This function is used to copy contents of file2 to file1
 void copyContents( char* file1 , char* file2 , char* file3 ) {
-
   FILE *fp1 = fopen(file1, "r");
   FILE *fp2 = fopen(file2, "r");
 
   // Open file to store the result
   FILE *fp3 = fopen(file3, "w");
   char c;
-
   if (fp1 == NULL || fp2 == NULL || fp3 == NULL){
-      puts("Could not open files");
-      exit(0);
-    }
+    puts("Could not open files");
+    exit(0);
+  }
+  // Copy contents of first file to file3.txt
+  while ((c = fgetc(fp1)) != EOF)
+  fputc(c, fp3);
 
-    // Copy contents of first file to file3.txt
-    while ((c = fgetc(fp1)) != EOF)
-    fputc(c, fp3);
-
-    // Copy contents of second file to file3.txt
+  // Copy contents of second file to file3.txt
   while ((c = fgetc(fp2)) != EOF)
-   fputc(c, fp3);
+  fputc(c, fp3);
 
-   fclose(fp1);
-   fclose(fp2);
-   fclose(fp3);
+  fclose(fp1);
+  fclose(fp2);
+  fclose(fp3);
 }
 
 void file_rename( char* oldFilename, char* newFilename) {
-    int ret = rename(oldFilename, newFilename);
+  int ret = rename(oldFilename, newFilename);
 
-    if(!ret)
-    {
-        printf("File name changed successfully %s to %s \n", oldFilename , newFilename);
-    }
-    else
-    {
-        perror("Error");
-    }
+  if(!ret){
+    printf("File name changed successfully %s to %s \n", oldFilename , newFilename);
+  }
+  else{
+    perror("Error");
+  }
 }
 
 // This function implements path command
 int main() {
-   char cwd_str[500];
-   // print current working directory
-   if (getcwd(cwd_str, sizeof(cwd_str)) != NULL) {
-       printf("Current directory : %s \n", cwd_str);
-   } else {
-       perror("getcwd() error");
-       //return ;
-   }
-   // print contents of file t2.txt
-   FILE * fptr;
-   char ch;
+  FILE * fptr;
+  char ch;
+  char cwd_str[500];
+  const char *directory;
+  // print current working directory
+  if (getcwd(cwd_str, sizeof(cwd_str)) != NULL) {
+    printf("Current directory : %s \n", cwd_str);
+  }
+  else {
+    perror("getcwd() error");
+  }
+  fptr = fopen("Dir0/t2.txt", "a");
 
-   fptr = fopen("Dir0/t2.txt", "r");
    // check for file opening
-   if(fptr == NULL)
-   {
-       //Unable to open file hence exit
-       printf("Unable to open file.\n");
-       printf("Please check whether file exists and you have read privilege.\n");
-       exit(EXIT_FAILURE);
-   }
+  if(fptr == NULL){
+    //Unable to open file hence exit
+    printf("Error writing to t2.txt\n");
+    exit(1);
+  }
+  else{
+    fprintf(fptr,"%s\n", cwd_str);
+  }
 
-   //File open success message
-   printf("t2.txt opened successfully. Reading file contents: \n\n");
+  fclose(fptr);
 
-   do
-   {
-       //Read single character from file
-       ch = fgetc(fptr);
-       // Print character read on console
-       putchar(ch);
-   } while(ch != EOF); // Repeat this if last read character is not EOF
-      fclose(fptr);
+  // rename t2.txt to path-info.txt
+  file_rename("Dir0/t2.txt","Dir0/path-info.txt");
+  copyContents("Dir0/tree.txt","Dir0/path-info.txt","Dir0/t3.txt");
+  file_rename("Dir0/t3.txt","Dir0/log.txt");
 
-       // rename t2.txt to path-info.txt
-       file_rename("Dir0/t2.txt","Dir0/path-info.txt");
-       copyContents("Dir0/tree.txt","Dir0/path-info.txt","Dir0/t3.txt");
-       file_rename("Dir0/t3.txt","Dir0/log.txt");
+  if (remove("Dir0/tree.txt") == 0)
+    printf(" In Path Command : deleted tree.txt successfully \n");
+  else
+    printf(" In Path Command: could not delete tree.txt \n");
 
-
-       if ( remove("Dir0/tree.txt") == 0 )
-         printf("Deleted tree.txt successfully \n");
-       else
-          printf("Can't delete tree.txt \n");
-
-       if ( remove("Dir0/path-info.txt") == 0 )
-          printf("Deleted path-info.txt successfully \n");
-       else
-          printf("Can't delete path.txt \n");
+  if (remove("Dir0/path-info.txt") == 0)
+    printf(" In Path Command : deleted path-info.txt successfully \n");
+  else
+    printf(" In Path Command: could not delete path.txt \n");
 }
